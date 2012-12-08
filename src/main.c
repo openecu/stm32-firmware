ee8f00c2d368ef912bb6566c7b2483b05dffd6a3
Sergey Fedotov <sergey89@gmail.com>
2012-12-08 20:09:00 +0400

    -

--- src/main.c
+++ src/main.c
@@ -1,10 +1,16 @@
 #include <stm32f4xx.h>
+#include "ecu.h"
 #include "actuators.h"
+
+extern ecu_t ecu;
 
 int main(void)
 {
+    actuators_init();
+
     for (;;)
     {
+        //fuel_pump_on();
         cooling_fan();
         aux();
     }
