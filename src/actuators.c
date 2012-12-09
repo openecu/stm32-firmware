90daebd4178ff93fd32f7a5b76660e43ff825293
Sergey Fedotov <sergey89@gmail.com>
2012-12-09 21:23:07 +0400

    -

--- src/actuators.c
+++ src/actuators.c
@@ -6,7 +6,6 @@ extern ecu_t ecu;
 void actuators_init(void)
 {
     ACTUATORS_OFF();
-    AUX_OFF();
 }
 
 void cooling_fan(void)
@@ -18,64 +17,5 @@ void cooling_fan(void)
     else if (ecu.sensors.ect < (ecu.config.cooling_fan_temp - ecu.config.cooling_fan_temp_hyst))
     {
         ACTUATOR_OFF(COOLING_FAN);
-    }
-}
-
-void water_pump(void)
-{
-    if (ecu.sensors.ect >= ecu.config.water_pump_temp)
-    {
-        ACTUATOR_ON(WATER_PUMP);
-    }
-    else if (ecu.sensors.ect < (ecu.config.water_pump_temp - ecu.config.water_pump_temp_hyst))
-    {
-        ACTUATOR_OFF(WATER_PUMP);
-    }
-}
-
-void aux(void)
-{
-    uint8_t i;
-    aux_config_t *aux;
-
-    for (i = 0; i < AUX_COUNT; i++)
-    {
-        aux = &ecu.config.aux[i];
-
-        if (!(aux->flags & AUX_FLAG_EN))
-        {
-            AUX_CH_OFF(i);
-        }
-        else
-        {
-            if (
-                (ecu.sensors.rpm >= aux->rpm_on)
-                && (ecu.sensors.ect >= aux->ect_on)
-            )
-            {
-                if ((aux->flags & AUX_FLAG_INV))
-                {
-                    AUX_CH_OFF(i);
-                }
-                else
-                {
-                    AUX_CH_ON(i);
-                }
-            }
-            else if (
-                (ecu.sensors.rpm <= aux->rpm_off)
-                && (ecu.sensors.ect <= aux->ect_off)
-            )
-            {
-                if ((aux->flags & AUX_FLAG_INV))
-                {
-                    AUX_CH_ON(i);
-                }
-                else
-                {
-                    AUX_CH_OFF(i);
-                }
-            }
-        }
     }
 }
--- src/actuators.h
+++ src/actuators.h
@@ -4,20 +4,12 @@
 #include <stm32f4xx.h>
 #include <inttypes.h>
 
-/* Actuators */
-
 #define ECU_RELAY_PIN   0
-#define CE_LAMP_PIN     1
-#define FUEL_PUMP_PIN   2
-#define COOLING_FAN_PIN 3
-#define WATER_PUMP_PIN  4
+#define FUEL_PUMP_PIN   1
+#define COOLING_FAN_PIN 2
 
 #define ACTUATORS_GPIO      GPIOA
-#define ACTUATORS_PORT_MASK ((1 << ECU_RELAY_PIN) \
-    | (1 << CE_LAMP_PIN) \
-    | (1 << FUEL_PUMP_PIN) \
-    | (1 << COOLING_FAN_PIN) \
-    | (1 << WATER_PUMP_PIN))
+#define ACTUATORS_PORT_MASK ((1 << ECU_RELAY_PIN) | (1 << FUEL_PUMP_PIN) | (1 << COOLING_FAN_PIN))
 
 #define ACTUATORS_OFF() ACTUATORS_GPIO->ODR &= ~ACTUATORS_PORT_MASK
 
@@ -46,30 +38,6 @@ static inline void fuel_pump_relay_off(void)
     ACTUATOR_OFF(FUEL_PUMP);
 }
 
-static inline void ce_lamp_on(void)
-{
-    ACTUATOR_ON(CE_LAMP);
-}
-
-static inline void ce_lamp_off(void)
-{
-    ACTUATOR_OFF(CE_LAMP);
-}
-
 void cooling_fan(void);
-
-void water_pump(void);
-
-/* Auxiliary outputs */
-#define AUX_GPIO        GPIOA
-#define AUX_PORT_OFFSET 8
-#define AUX_PORT_MASK   (0xFF << AUX_PORT_OFFSET)
-
-#define AUX_OFF()   AUX_GPIO->ODR &= ~(1 << AUX_PORT_OFFSET);
-
-#define AUX_CH_ON(num)     AUX_GPIO->ODR |= (1 << (num + AUX_PORT_OFFSET))
-#define AUX_CH_OFF(num)    AUX_GPIO->ODR &= ~(1 << (num + AUX_PORT_OFFSET))
-
-void aux(void);
 
 #endif
