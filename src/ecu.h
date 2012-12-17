88fc75dcec85f1c816ee2367fbc39ca7dc675712
Sergey Fedotov <sergey89@gmail.com>
2012-12-18 00:44:45 +0400

    -

--- src/ecu.h
+++ src/ecu.h
@@ -4,15 +4,19 @@
 #include "config.h"
 #include "sensors.h"
 
-#define MODE_CRANKING   0x01
-#define MODE_IDLING     0x02
-#define MODE_POWER      0x03
+#define MODE_CRANK  0x01
+#define MODE_IDLE   0x02
+#define MODE_POWER  0x03
+
+#define STATUS1_AFTERSTART_ENRICH   0x00000001
+#define STATUS1_WARMUP_ENRICH       0x00000002
 
 typedef struct
 {
     config_t config;
     sensors_t sensors;
     uint8_t mode;
+    uint32_t status1;
     uint32_t cycle_time;
 } ecu_t;
 
--- src/injection.c
+++ src/injection.c
@@ -7,28 +7,30 @@ uint16_t crank_cycles;
 
 void calc_pw(void)
 {
-    uint32_t load, fuel, pw;
-    uint16_t air_flow, afr;
+    uint32_t load, fuel, pw, enrich;
+    uint16_t afr;
 
     // Cranking mode
-    if (ecu.mode == MODE_CRANKING)
+    if (ecu.mode == MODE_CRANK)
     {
         load = table1d_lookup(ecu.sensors.ect, CRANK_TEMP_SCALE_SIZE,
             (int16_t*)ecu.config.crank_temp_scale, (int16_t*)ecu.config.crank_load);
         uint16_t crank_cycle_enrich = table1d_lookup((crank_cycles++), CRANK_CYCLE_SCALE_SIZE, 
             (int16_t*)ecu.config.crank_cycle_scale, (int16_t*)ecu.config.crank_cycle_enrich);
 
-        fuel = (load * 100) / ecu.config.crank_afr;
-        fuel = (fuel * crank_cycle_enrich) / 100;
+        afr = ecu.config.crank_afr;
+        enrich = crank_cycle_enrich;
+
+        ecu.status1 |= STATUS1_AFTERSTART_ENRICH;
     }
     else
     {
-        air_flow = table1df_lookup(ecu.sensors.maf, MAFS_VOLTAGE_SCALE_SIZE,
+        uint16_t air_flow = table1df_lookup(ecu.sensors.maf, MAFS_VOLTAGE_SCALE_SIZE,
             0, 1024, (int16_t*)ecu.config.mafs_calibration);
         load = air_flow * ecu.cycle_time;
 
         // Idling mode
-        if (ecu.mode == MODE_IDLING)
+        if (ecu.mode == MODE_IDLE)
         {
             afr = ecu.config.idle_afr;
         }
@@ -39,11 +41,41 @@ void calc_pw(void)
                 (int16_t*)ecu.config.table_afr_rpm_scale, (int16_t*)ecu.config.table_afr_load_scale, (int16_t*)ecu.config.table_afr);
         }
 
+        // Warm-Up enrichment
         uint16_t warmup_enrich = table1d_lookup(ecu.sensors.ect, WARMUP_TEMP_SCALE_SIZE, 
             (int16_t*)ecu.config.warmup_temp_scale, (int16_t*)ecu.config.warmup_enrich);
+        enrich = warmup_enrich;
+
+        if (warmup_enrich == 1000)
+        {
+            ecu.status1 &= ~STATUS1_WARMUP_ENRICH;
+        }
+
+        // Afterstart enrichment
+        if ((ecu.status1 & STATUS1_AFTERSTART_ENRICH))
+        {
+            uint16_t afterstart_enrich = table1d_lookup(ecu.sensors.ect, AFTERSTART_TEMP_SCALE_SIZE, 
+                (int16_t*)ecu.config.afterstart_temp_scale, (int16_t*)ecu.config.afterstart_enrich);
+            afterstart_enrich -= ecu.config.afterstart_enrich_decay * (crank_cycles++);
+
+            if (afterstart_enrich > warmup_enrich)
+            {
+                enrich = afterstart_enrich;
+            }
+            // Leave afterstart enrichment
+            else
+            {
+                if (warmup_enrich != 1000)
+                {
+                    ecu.status1 |= STATUS1_WARMUP_ENRICH;
+                }
+                
+                ecu.status1 &= ~STATUS1_AFTERSTART_ENRICH;
+            }
+        }
 
         fuel = (load * 100) / afr;
-        fuel = (fuel * warmup_enrich) / 100;
+        fuel = (fuel * enrich) / 1000;
     }
 
     pw = fuel / ecu.config.inj_mult;
